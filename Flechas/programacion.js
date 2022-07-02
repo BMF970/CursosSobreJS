e93ea1473ff6a226b63b14cf88c28fd375cdc218
BMF970 <brianmartines119@yahoo.com>
2022-07-01 21:30:01 -0500

    Inicio de proyecto Pizarra Magica

--- Flechas/programacion.js
+++ Flechas/programacion.js
@@ -4,34 +4,40 @@ var teclas = {
     LEFT: 37,
     RIGHT: 39
 };
-
-document.addEventListener("keyup", dibuj/arTeclado);
+var x = 500;
+var y = 500;
+document.addEventListener("keydown", dibujarTeclado);
 var lienzo = document.getElementById("areaDeDibujo");
 var papel = lienzo.getContext("2d");/*
 crearLinea("black", 1, 1, 1,999);
 crearLinea("black", 1, 499, 999, 999);
 crearLinea("black", 999, 999, 999, 1);
 crearLinea("black", 999, 999, 1, 1);*/
-dibujarLinea("red", 100, 100, 200, 200);
+dibujarLinea("red", 499, 499, 501, 501, papel);
 
 function dibujarTeclado(evento){
-    switch(evento.keycode)
+    var blueColor = "red";
+    var movimiento = 10;
+    switch(evento.keyCode)
     {
-        case teclas.DOWN:
-            console.log("pa abajo");
-            break;
         case teclas.UP:
-            console.log("pa arriba");
+            dibujarLinea(blueColor, x, y, x, y - movimiento, papel);
+            y = y - movimiento;
+            break;
+        case teclas.DOWN:
+            dibujarLinea(blueColor, x, y, x, y + movimiento, papel);
+            y = y + movimiento; 
             break;
         case teclas.LEFT:
-            console.log("Pa la izquierda");
+            dibujarLinea(blueColor, x, y, x - movimiento, y, papel);
+            x = x - movimiento;
             break;
         case teclas.RIGHT:
-            console.log("derecha");
+            dibujarLinea(blueColor, x, y, x + movimiento, y, papel);
+            x = x + movimiento;
             break;
         default:
             console.log("Otra Tecla");
-
     }
 }
 
@@ -56,10 +62,11 @@ function dibujarTeclado(evento){
 
 
 
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
 {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3;
     lienzo.moveTo(xinicial, xfinal);
     lienzo.lineTo(xfinal, yfinal);
     lienzo.stroke();
