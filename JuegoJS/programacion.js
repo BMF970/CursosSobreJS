e8c910fbcb0c3c186819dc31e3115c9beb75f4fa
BMF970 <brianmartines119@yahoo.com>
2022-07-03 13:33:53 -0500

    Correccion de errores y primera impresion de objetos

--- JuegoJS/programacion.js
+++ JuegoJS/programacion.js
@@ -1,3 +1,48 @@
+var vp = document.getElementById("villaPlatzi");
+var papel = vp.getContext("2d");
+
+var fondo = {
+    url: "archivos/Granja.png",
+    cargaOk: false
+};
+
+var vaca = {
+    url: "archivos/vaca.png",
+    cargaOk: false
+};
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+
+function dibujar(){
+    if(fondo.cargaOk){
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
+    if(vaca.cargaOk){
+        papel.drawImage(vaca.imagen, 100, 100);
+    }
+}
+
+function cargarFondo(){
+    fondo.cargaOk = true;
+    dibujar();
+}
+
+function cargarVacas(){
+    vaca.cargaOk = true;
+    dibujar();
+}
+
+
+
+
+
 
 for(var i = 0; i < 10; i++){
     z = aleatorio(10, 20);
