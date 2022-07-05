819b9026f7dcabe4faca0b291a24435245520215
BMF970 <brianmartines119@yahoo.com>
2022-07-05 09:43:09 -0500

    Creando funciones y parametros para imprimir tambien cerdos y pollos de manera aleatoria

--- JuegoJS/programacion.js
+++ JuegoJS/programacion.js
@@ -1,6 +1,6 @@
 var vp = document.getElementById("villaPlatzi");
 var papel = vp.getContext("2d");
-var cantidad = aleatorio(1,50);
+var cantidad = aleatorio(1, 10);
 
 var fondo = {
     url: "archivos/Granja.png",
@@ -12,6 +12,16 @@ var vaca = {
     cargaOk: false
 };
 
+var cerdo = {
+    url: "archivos/cerdo.png",
+    cargaOk: false
+};
+
+var pollo = {
+    url: "archivos/pollo.png",
+    cargaOk: false
+};
+
 fondo.imagen = new Image();
 fondo.imagen.src = fondo.url;
 fondo.imagen.addEventListener("load", cargarFondo);
@@ -20,18 +30,40 @@ vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVacas);
 
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
 
 function dibujar(){
     if(fondo.cargaOk){
         papel.drawImage(fondo.imagen, 0, 0);
     }
     if(vaca.cargaOk){
-        for( var v = 0; v < 10; v++)
+        for( var v = 0; v < cantidad; v++)
 		{
 			var x = aleatorio (0, 420);
 			var y = aleatorio (0, 420);
 			papel.drawImage(vaca.imagen , x , y);
-			console.log(v);
+		}
+    }
+    if(cerdo.cargaOk){
+        for( var v = 0; v < cantidad; v++)
+		{
+			var x = aleatorio (0, 420);
+			var y = aleatorio (0, 420);
+			papel.drawImage(cerdo.imagen , x , y);
+		}
+    }
+    if(pollo.cargaOk){
+        for( var v = 0; v < cantidad; v++)
+		{
+			var x = aleatorio (0, 420);
+			var y = aleatorio (0, 420);
+			papel.drawImage(pollo.imagen , x , y);
 		}
     }
 }
@@ -46,10 +78,15 @@ function cargarVacas(){
     dibujar();
 }
 
+function cargarCerdos(){
+    cerdo.cargaOk = true;
+    dibujar();
+}
 
-
-
-
+function cargarPollos(){
+    pollo.cargaOk = true;
+    dibujar();
+}
 
 for(var i = 0; i < 10; i++){
     z = aleatorio(10, 20);
@@ -59,8 +96,7 @@ for(var i = 0; i < 10; i++){
 
 var z;
 
-function aleatorio(min, max)
-{
+function aleatorio(min, max){
     var resultado;
     resultado = Math.floor(Math.random() * (max - min + 1)) + min;
     return resultado;
