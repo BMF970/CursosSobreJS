a6319f2c603d56d9ce9e287cc9cc789b465c850c
BMF970 <brianmartines119@yahoo.com>
2022-07-05 19:44:26 -0500

    codigo solucionado, solo falta hacer el merge a la rama master

--- JuegoJS/programacion.js
+++ JuegoJS/programacion.js
@@ -1,105 +1,140 @@
 var vp = document.getElementById("villaPlatzi");
 var papel = vp.getContext("2d");
-var cantidadVacas = aleatorio(3, 15);
-var cantidadCerdos = aleatorio(4, 12);
-var cantidadPollos = aleatorio(3, 60);
+var teclas = {
+	UP: 38,
+	DOWN: 40,
+	LEFT: 37,
+	RIGHT: 39
+};
 
 
 var fondo = {
-    url: "archivos/Granja.png",
-    cargaOk: false
+	url: "archivos/Granja.png",
+	cargaOK: false
 };
 
 var vaca = {
-    url: "archivos/vaca.png",
-    cargaOk: false,
-    x: [],
-    y: []
+	url: "archivos/vaca.png",
+	cargaOK: false,
+	x : [],//Creo una variable X y Y para vaca y pollo en arreglo y la de cerdo la inicializo en cero
+	y : []
 };
 
 var cerdo = {
-    url: "archivos/cerdo.png",
-    cargaOk: false,
-    x: 0,
-    y
+	url: "archivos/cerdo.png",
+	cargaOK: false,
+	x : 0,
+	y : 0
 };
 
 var pollo = {
-    url: "archivos/pollo.png",
-    cargaOk: false,
-    x: [],
-    y: []
+	url: "archivos/pollo.png",
+	cargaOK: false,
+	x : [],
+	y : []
 };
+ 
+ 
+var cantidad = aleatorio(1,50);
 
-fondo.imagen = new Image();
+fondo.imagen = new Image();//creamos objeto
 fondo.imagen.src = fondo.url;
-fondo.imagen.addEventListener("load", cargarFondo);
+fondo.imagen.addEventListener("load", cargarFondo);//escuchamos el evento de carga del navegador
 
 vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
-vaca.imagen.addEventListener("load", cargarVacas);
+vaca.imagen.addEventListener("load", cargarVaca);
 
 cerdo.imagen = new Image();
 cerdo.imagen.src = cerdo.url;
-cerdo.imagen.addEventListener("load", cargarCerdos);
+cerdo.imagen.addEventListener("load", cargarCerdo);
 
 pollo.imagen = new Image();
 pollo.imagen.src = pollo.url;
-pollo.imagen.addEventListener("load", cargarPollos);
+pollo.imagen.addEventListener("load", cargarPollo);
 
-function dibujar(){
-    if(fondo.cargaOk){
-        papel.drawImage(fondo.imagen, 0, 0);
-    }
-    if(vaca.cargaOk){
-        for( var v = 0; v < cantidadVacas; v++)
+function cargarFondo ()//cuando ocurre el evento, se llama a estas funciones para que a su vez llamen la funcion de dibujar
+{
+	fondo.cargaOK = true;
+	dibujar();
+}
+ 
+ function cargarVaca ()
+{
+	vaca.cargaOK = true;
+	dibujar();
+}
+
+function cargarCerdo ()
+{
+	cerdo.cargaOK = true;
+	dibujar();
+}
+
+function cargarPollo ()
+{
+	pollo.cargaOK = true;
+	dibujar();
+}
+
+function dibujar()//aqui se evalua cual imagen ha cargado, para que siga el orden de carga que queremos
+{
+
+	if(fondo.cargaOK)
+	{
+		papel.drawImage(fondo.imagen , 0 , 0);
+	}
+	if(vaca.cargaOK)//si el objeto de la vaca ya cargo se ejecuta el ciclo en donde se colocan cantidades y posiciones aleatorias
+		//tambien se guardan en el arreglo las posiciones aleatorias que se generaron para posteriormente saber redibujar el objeto cuando
+	//movamos el cerdo en el teclado
+	{
+		for( var v = 0; v < cantidad; v++)
 		{
-			var x = (aleatorio (0, 7)*60);
-			var y = (aleatorio (0, 7)*60);
+			var x = (aleatorio (0,7)*60);
+			var y = (aleatorio (0,7)*60);
+			vaca.x[v] = x;
+			vaca.y[v] = y;
 			papel.drawImage(vaca.imagen , x , y);
+			console.log(cantidad);
 		}
-    }
-    if(cerdo.cargaOk){
-        for( var v = 0; v < cantidadCerdos; v++)
+		
+	}
+	if(cerdo.cargaOK)
+	{	
+		var x = (aleatorio (0,7)*60);
+		var y = (aleatorio (0,7)*60);
+		cerdo.x = x;
+		cerdo.y = y;
+		papel.drawImage(cerdo.imagen , x , y);
+		
+	}
+	if(pollo.cargaOK)
+	{
+		for(var p =0; p <cantidad ; p++)
 		{
-			var x = (aleatorio (0, 7)*60);
-			var y = (aleatorio (0, 7)*60);
-			papel.drawImage(cerdo.imagen , x , y);
-		}
-    }
-    if(pollo.cargaOk){
-        for( var v = 0; v < cantidadPollos; v++)
-		{
-			var x = (aleatorio (0, 8)*70);
-			var y = (aleatorio (0, 8)*70);
+			var x = (aleatorio (0,7)*60);
+			var y = (aleatorio (0,7)*60);
+			pollo.x [p] = x;
+			pollo.y [p] = y;
 			papel.drawImage(pollo.imagen , x , y);
+			console.log(cantidad);
 		}
-        
-    }
+		
+	}
+
 }
 
-function cargarFondo(){
-    fondo.cargaOk = true;
-    dibujar();
+
+function aleatorio(min , maxi)
+{
+	var resultado;
+	resultado =  Math.floor(Math.random() * (maxi - min + 1)) + min;
+	return resultado;
 }
 
-function cargarVacas(){
-    vaca.cargaOk = true;
-    dibujar();
-}
-
-function cargarCerdos(){
-    cerdo.cargaOk = true;
-    dibujar();
-}
-
-function cargarPollos(){
-    pollo.cargaOk = true;
-    dibujar();
-}
-
-function moverCerdo(x, y){
-    papel.drawImage(cerdo.imagen, x, y);
+function moverCerdo(x,y)
+{
+	papel.drawImage(cerdo.imagen,x,y);
 }
 
 function dibujarOtraVez()//redibujamos los objetos, solo que sin el ciclo y utilizando los numero aleatorios de la funcion dibujar
@@ -126,16 +161,12 @@ function dibujarOtraVez()//redibujamos los objetos, solo que sin el ciclo y util
 	 }
 }
 
-function aleatorio(min, max){
-    var resultado;
-    resultado = Math.floor(Math.random() * (max - min + 1)) + min;
-    return resultado;
-}
-
 document.addEventListener("keyup", moverTecla);//identificamos el evento del teclado y restamos o sumamos posicion, segun la tecla 
-	function moverTecla(evento){
-			var movimiento = 5;
-			switch (evento.keyCode){
+	function moverTecla(evento)
+	{
+			var movimiento = 15;
+			switch (evento.keyCode)
+		{
 			case teclas.UP:
 				dibujarOtraVez();
 				moverCerdo(cerdo.x,cerdo.y);
@@ -161,15 +192,3 @@ document.addEventListener("keyup", moverTecla);//identificamos el evento del tec
 			break;
 		}	
 	}	
-
-document.write("Hay un total de " + cantidadVacas + " vacas");
-document.write("</br>Hay un total de " + cantidadCerdos + " cerdos");
-document.write("Hay un total de " + cantidadPollos + " pollos");
-/*for(var i = 0; i < 10; i++){
-    z = aleatorio(10, 20);
-    document.write(z + ", ");
-}
-
-
-var z;
-      
