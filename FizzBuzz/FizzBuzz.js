e28be0bebd917348f9d8f7b621ecef3d9456c625
BMF970 <brianmartines119@yahoo.com>
2022-07-06 09:55:39 -0500

    Terminando el proyecto

--- FizzBuzz/FizzBuzz.js
+++ FizzBuzz/FizzBuzz.js
@@ -1,10 +1,28 @@
 var i;
 var numeros = 100;
+var divisible = false;
 for(i = 1; i <= numeros; i++){
-    if(i % 3 == 0){
-        document.write("Fizz");
-     }else{
+    if(esDivisible(i, 3)){
+    //var divisible = false;
+        document.write(" Fizz");
+        //divisible = true;
+    }
+
+     if(esDivisible(i, 5)){
+        document.write(" Buzz");
+        //divisible = true;
+    }
+     
+     if(!esDivisible(i, 3) && !esDivisible(i, 5)){
         document.write(i);
-     }
+    }
     document.write("<br/>");
+}
+
+function esDivisible(num, divisor){
+    if (num % divisor == 0){
+        return true;
+    }else{
+        return false;
+    }
 }
