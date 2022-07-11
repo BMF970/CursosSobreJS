503947cf087dd75cb7a2dc12b4bad0b5f2d05419
BMF970 <brianmartines119@yahoo.com>
2022-07-11 09:17:16 -0500

    Aprendiendo sobre clases y arrays :)

---
+++ Pakiman/programacionPakiman.js
@@ -0,0 +1,3 @@
+class pakiman{
+    Name
+}
---
+++ objetosYClases.js
@@ -0,0 +1,7 @@
+class nuevaClase{//la clase es la definicion de un objeto
+/*Con esto estamos creando un objeto, la definicion de un objeto...*/
+    constructor(){/*es como escribir una funcion pero esta no tiene nombre, es parte de la clase*/
+        /*En las clases no funcionan las variables, tienen que ser en el constructor*/
+        this.mensaje = "Este es un mensaje";
+    }
+}
