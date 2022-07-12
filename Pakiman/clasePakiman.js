93066a9a498b31a098a85e3004c238a1f5c16de4
BMF970 <brianmartines119@yahoo.com>
2022-07-12 17:12:24 -0500

    Archivo Pakiman listo, editado con comentarios para explicar su funcionamiento

--- Pakiman/clasePakiman.js
+++ Pakiman/clasePakiman.js
@@ -1,23 +1,26 @@
-class Pakiman
+/*Por que este archivo? Principalmente para ser mas profesional y mejores practicas, solamente hay que recordar
+que tenemos que insertarlo antes de el archivo programacionPakiman.js, por que ese archivo usa este objeto*/
+class Pakiman//Este objeto es la plantilla para crear pakimanes
 {
-    constructor(n, v, a)
+    constructor(n, h, a)//aqui ponemos las iniciales de los atributos basicos de los pakimanes
     {
-    this.image = new Image();
-    this.name = n;
-    this.health = v;
+    this.image = new Image();//esto es para agregar la imagen
+    this.name = n;//atributos basicos
+    this.health = h;
     this.atack = a;
-
-    this.image.src = images[this.name];
-    }
+    this.image.src = images[this.name];/*Este en particular es para colocar la imagen
+    aqui arriba ^ es la ubicacion, ^ y esto redirecciona el archivo al nombre, pues 
+    cada vez que mandemos traer el nombre la imagen aparecera*/}
     hablar()
     {
-        alert(this.name);
+        alert(this.name);//funcion que podemos traer para emular como habla el pakiman
     }
 
-    show(){
-        document.body.appendChild(this.image);
-        document.write("<br/><strong>" + this.name + "</strong><br/>");
-        document.write("Health: " + this.health + "<br/>");
-        document.write("atack: " + this.atack + "<hr/>");
-    }
+    show(){//Esta otro atributo esta enfocado para poner la informacion del animal
+        document./*        */body./*                           */appendChild/*                                     */(this.image);/*Este metodo es para insertar la imagen, pues necesita un metodo especial para colocar una imagen
+        ^ parte de la pagina, ^ especificamos el body (importante), ^ metodo enfocado en crear una especie nueva en html, ^ colocamos el nombre gracias al array asociativo que nos traera la imagen cuando mandemos traer el nombre*/
+        document.write("<br/><strong>" + this.name + "</strong><br/>");//Agregamos el nombre
+        document.write("Health: " + this.health + "<br/>");//Agregamos la vida/salud
+        document.write("atack: " + this.atack + "<hr/>");//Agregamos el ataque
+    }//                                            ^ esta madre sirve para poner la linea y crear un salto de linea para evitar que se empalme la informacion
 }
--- Pakiman/programacionPakiman.js
+++ Pakiman/programacionPakiman.js
@@ -1,14 +1,16 @@
-HEAD
-var images =[];
+var images =[];//Array asociativo que usamos para poner la imagen anclada al nombre y que cada vez que mandemos llamar el nombre se presente la imagen tambien
 images["Cauchin"] = "vaca.png";
 images["Pokacho"] = "pollo.png";
 images["Tocinauro"] = "cerdo.png";
  
-var coleccion = [];
-coleccion.push(new Pakiman("Cauchin", 100, 30));
+var coleccion = [];//usamos este array para evitar estar poniendo muchos console log, pues de lo contrario tendriamos que colocar una linea por cada animal que queramos llamar
+coleccion.push(/*                        */new Pakiman("Cauchin", 100, 30));
+//^ para agregar al final cada nuevo pakiman, ^ nos permite agregar directamente los pakimanes 
 coleccion.push(new Pakiman("Pokacho", 80, 50));
 coleccion.push(new Pakiman("Tocinauro", 120, 40));
 
+//El siguiente buucle nos permite mostrar todos los pakimanes pues...
 for (var allPakiman of coleccion){
-    allPakiman.show();
+// tiene esta cosa  ^^ que nos permite recorrer cada objeto de el array "coleccion"
+    allPakiman.show();//metodo usado para imprimir en el documento toda la informacion
 }
