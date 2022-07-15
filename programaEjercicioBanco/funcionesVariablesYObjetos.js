4380aa5ae00a236fb10e3af8feb84f53e1cee4cb
BMF970 <brianmartines119@yahoo.com>
2022-07-14 22:07:55 -0500

    Algoritmo funcional, solo falta pasarlo a html y decorarlo

--- programaEjercicioBanco/funcionesVariablesYObjetos.js
+++ programaEjercicioBanco/funcionesVariablesYObjetos.js
@@ -4,23 +4,47 @@ class Billete{
         this.cantidad = c;
     }
 }
-var b = document.getElementById("extraer");
-b.addEventListener("click", entregarDinero);
 
 var caja = [];
 var entregado = [];
-caja.push(new Billete(50, 3));
-caja.push(new Billete(20, 2));
-caja.push(new Billete(10, 2));
-var dinero = 210;
+caja.push(new Billete(50, 10));
+caja.push(new Billete(20, 30));
+caja.push(new Billete(10, 10));
+var dinero = 0;
+var div = 0;
 var papeles = 0;
-var div = 0;//(dinero, billete)
 
-function entregarDinero(){
-    for(var bi of caja) {
-        if(dinero > 0){
-//div = Math.floor(dinero/bi.valor);
-            console.log(bi);
-        }        
+
+function entregarDinero()
+{
+    var t = document.getElementById("dinero");
+    dinero = parseInt(t.value);
+    for(var bi of caja)
+    {
+        if(dinero > 0)
+        {
+            div = Math.floor(dinero / bi.valor);
+            if(div > bi.cantidad)
+            {
+                papeles = bi.cantidad;
+            }else
+            {
+                papeles = div;    
+            }
+            entregado.push(new Billete(bi.valor, papeles));
+            dinero = dinero - (bi.valor * papeles);
+        }
+    }
+    if (dinero > 0){
+        document.write("<tr/>Soy un cajero pobre y no tengo dinero");
+
+    }else{
+        for(var e of entregado);
+        document.write(e.cantidad + "billetes de $" + e.valor + "<br/>");
     }
 }
+
+
+var b = document.getElementById("extraer");
+//parseInt("dinero");
+b.addEventListener("click", entregarDinero);
