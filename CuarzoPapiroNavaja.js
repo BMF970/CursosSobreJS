24d3cd29a58f9e1374c0cf771ae994fc0ac954f1
BMF970 <brianmartines119@yahoo.com>
2022-07-22 09:17:25 -0500

    Agregando funciones y mejorando el codigo de piedra papel o tijera

--- CuarzoPapiroNavaja.js
+++ CuarzoPapiroNavaja.js
@@ -1,18 +1,35 @@
-var opc1 = "Piedra";
-var opc2 = "Papel";
-var opc3 = "Tijera";
+var jugador = 0;
+var pc = aleatorio(1,3);
 
-//function
-var game = function(user, cpu){
-    if(user === opc1 && cpu === opc2){
-        console.log("El usuario gano con " + opc1);
-    }else if (user === opc2 && cpu === opc1){
-        console.log("El usuario gano con " + opc2);
-    }else if(user === opc3 && cpu === opc2){
-        console.log("El usuario gano con " + opc3);
-    }else if(user === cpu){
-        console.log("Empate")    
+function aleatorio(min, max){return Math.floor(Math.random() * (max - min + 1) + min)}
+
+function eleccion(jugada){
+    let resultado = "";
+    if(jugada == 1){
+        resultado = "Piedra";
+    }else if (jugada == 2){
+        resultado = "Papel";
+    }else if(jugada == 3){
+        resultado = "Tijera";
     }else{
-        console.log("La consola gano con " cpu);
+        resultado = "Mal elegido";
     }
+    return resultado;
+}
+
+jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera");
+
+alert("PC elige: " + eleccion(pc));
+alert("Tu elijes: " + eleccion(jugador));
+
+if(pc == jugador){
+    alert("Empate");
+}else if(jugador ==1 && pc==3){
+    alert("ganaste");
+}else if(jugador==2 && pc==1){
+    alert("ganaste");
+}else if(jugador==3 && pc==1){
+    alert("Ganaste");
+}else{
+    alert("Perdiste");
 }
