dc91effee4bf8eee1489f88d269ac5ca9723bc78
BMF970 <brianmartines119@yahoo.com>
2022-07-25 20:36:44 -0500

    Terminando el codigo de javascript, siguiente paso css

--- mokepon/programacion.js
+++ mokepon/programacion.js
@@ -4,6 +4,12 @@ let vidasJugador=3
 let vidasEnemigo=3
 
 function iniciarJuego(){
+    let sectionSeleccionarAtaque = document.getElementById(`seleccionar-ataque`)
+    sectionSeleccionarAtaque.style.display = `none`
+
+    let sectionReiniciar = document.getElementById(`reiniciar`)
+    sectionReiniciar.style.display = `none`
+
     let botonMascotaJugador = document.getElementById(`boton-mascota`);
     botonMascotaJugador.addEventListener(`click`, seleccionarMascotaJugador);    
     let botonFuego = document.getElementById(`boton-fuego`);
@@ -18,6 +24,12 @@ function iniciarJuego(){
 }
 
 function seleccionarMascotaJugador(){
+    let sectionSeleccionarMascota = document.getElementById(`seleccionar-mascota`)
+    sectionSeleccionarMascota.style.display = `none`
+
+    let sectionSeleccionarAtaque = document.getElementById(`seleccionar-ataque`)
+    sectionSeleccionarAtaque.style.display = `block`
+    
     let inputHipodoge = document.getElementById(`hipodoge`)
     let inputCapipepo = document.getElementById(`capipepo`)
     let inputRatigueya = document.getElementById(`ratigueya`)
@@ -132,6 +144,9 @@ function createFinal(resultadoFinal) {
     botonAgua.disabled = true
     let botonTierra = document.getElementById(`boton-tierra`);
     botonTierra.disabled = true
+    
+    let sectionReiniciar = document.getElementById(`reiniciar`)
+    sectionReiniciar.style.display = `block`
 }
 
 function reiniciarJuego() {
