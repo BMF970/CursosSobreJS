6b4ef8a8a1ba3753aed99e26dda56ac5bd67a395
BMF970 <brianmartines119@yahoo.com>
2022-08-01 17:59:11 -0500

    Practica, reloj funcional en DOM

---
+++ reloj/reloj.js
@@ -0,0 +1,19 @@
+let hours = 0;
+let min = 0;
+let seconds = 0;
+document.getElementById("horas")
+document.getElementById("minutos")
+document.getElementById("segundos")
+for (seconds = 00; seconds <= 60; seconds++) {
+    console.log(seconds)
+}
+for(min = 0; min <= 60; min ++){
+    console.log(min)
+}
+for(hours = 0; hours <= 24; hours++){
+    console.log(hours)
+}
+
+
+    /*
+console.log(seconds)*/
